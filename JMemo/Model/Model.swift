b8d72924ffd4da459484d7b89f835adf73b9b26f
juhwacheol <juhwacheol@gmail.com>
2021-06-18 06:28:17 +0900

    memo app

--- JMemo/Model/Model.swift
+++ JMemo/Model/Model.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+//class Memo {
+//    var content: String
+//    var insertDate: Date
+//
+//    init(content: String){
+//        self.content = content
+//        insertDate = Date()
+//    }
+//    static var dummyMemoList = [
+//        Memo(content: "Ju Ging Ging"),
+//        Memo(content: "Ging ba+❤️")
+//    ]
+//}
--- JMemo/UIVIewController+Alert.swift
+++ JMemo/UIVIewController+Alert.swift
@@ -5,4 +5,15 @@
 //  Created by HWACHEOL JU on 2021/06/08.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
